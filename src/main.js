245e31d874c284785c9a9400a88802c7e22404d5
smallwhitezhou <905328865@qq.com>
2017-04-11 23:43:41 +0800

    表格

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/stylesheets/reset01.css';
 //引入字体图标库
 import  './assets/libs/Font-Awesome-3.2.1/css/font-awesome.min.css';
 import './assets/libs/iconfont/iconfont.css';
+
 import Vue from 'vue';
 import App from './App';
 import router from './router';
