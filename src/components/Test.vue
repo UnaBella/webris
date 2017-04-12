f64b12b471a898d5d3a389f50e5a2207f30b8e7f
LittleWhiteZhou <905328865@qq.com>
2017-04-12 14:41:10 +0800

    LittleWhiteZhou

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -3,7 +3,10 @@
     <!--按钮-->
     <button class="btn">default</button>
     <button class="btn"
-            disabled>disabled</button>
+            disabled>disabled
+
+
+    </button>
     <br>
     <br>
     <!--提示框-->
@@ -27,357 +30,121 @@
     </div>
     <br>
     <!--表格-->
-    <div id="table-container">
-      <div class="table-lt">
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <div>
-                  header-left-top
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="table-top">
-        <div class="table-mask">
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <div>topTitle1</div>
-                </td>
-                <td>
-                  <div>topTitle2</div>
-                </td>
-                <td>
-                  <div>topTitle3</div>
-                </td>
-                <td>
-                  <div>topTitle4</div>
-                </td>
-                <td>
-                  <div>topTitle5</div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div class="table-left">
-        <div class="table-mask">
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <div>leftTitle1</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>leftTitle2</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>leftTitle3</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>leftTitle4</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>leftTitle5</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>leftTitle6</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>leftTitle7</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>leftTitle8</div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div class="table-right">
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <div>uoifdfdfuiuiouiouuoifdfdfuiuiouiouuoifdfdfuiuiouiouuoifdfdfuiuiouioud</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>uoiuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiouuoifdfdfuiuiouiouuoifdfdfuiuiouiouuoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>uoiuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>uoiuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>uoiuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>uoiuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>uoiuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>uoiuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoifdfdfuiuiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiufdsfsdfiouiou</div>
-              </td>
-              <td>
-                <div>uoiuiusiouiou</div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-  
+    <div class="table">
+      <table>
+        <thead>
+        <tr>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+          <th>6</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td></tr>
+        </tbody>
+      </table>
     </div>
 
   </div>
 </template>
 
 <script>
-import '../assets/libs/jquery1.91.min.js'
+  import '../assets/libs/jquery1.91.min.js'
 
-export default {
-  data() {
-    return {}
-  },
-  props: [],
-  components: {},
-  mounted(){
-var container = $("#table-container"),
-            ltTable = container.find(".table-lt"),
-            topTable = container.find(".table-top"),
-            leftTable = container.find(".table-left"),
-            rightTable = container.find(".table-right"),
-  
-            containerWidth = 0,
-            containerHeight =0,
-  
-            ltTableWidth = 0,
-            ltTableHeight = 0,
-  
-            widthJson = {},
-  
-            timerLT = null;
-  
-        ltTableWidth = ltTable.width();
-        ltTable.width(ltTableWidth);
-        topTable.css("marginLeft",ltTableWidth);
-        leftTable.width(ltTableWidth);
-        rightTable.css("marginLeft",ltTableWidth);
-        ltTableHeight = ltTable.height();
-        leftTable.css("marginTop",ltTableHeight+"px");
-        rightTable.css("marginTop",ltTableHeight+"px");
-  
-        containerHeight = container.height();
-        containerWidth = container.width();
-        topTable.width(containerWidth - ltTableWidth - (container.innerWidth() - container[0].clientWidth));
-        leftTable.height(containerHeight - ltTableHeight - (container.innerHeight() - container[0].clientHeight));
-  
-        // figure out the width of each DIV in TD  --start
-        function setDivWidth(obj){
-          $(obj).find("div").each(function(index){
-            if(!widthJson[index]){
-              widthJson[index] = 0;
-            }
-            if(widthJson[index]<$(this).width()) {
-              widthJson[index] = $(this).width();
-            }
-          });
-        }
-        topTable.find("tr").each(function(){
-          setDivWidth(this);
-        });
-        rightTable.find("tr").each(function(){
-          setDivWidth(this);
-        });
-  
-        topTable.find("tr:first div").each(function(index){
-          $(this).width(widthJson[index]);
-        });
-        rightTable.find("tr:first div").each(function(index){
-          $(this).width(widthJson[index]);
-        });
-        // figure out the width of each DIV in TD  --end
-  
-        container.scroll(function(){
-          var currentScrollTop = $(this).scrollTop(),
-              currentScrollLeft = $(this).scrollLeft();
-          topTable.find(".table-mask").css("left", -currentScrollLeft+"px");
-          leftTable.find(".table-mask").css("top", -currentScrollTop+"px");
-  
-        });
-  
-        $(document).scroll(function(){
-          var currentScrollTop = $(this).scrollTop(),
-              currentScrollLeft = $(this).scrollLeft();
-          ltTable.css("marginTop", -currentScrollTop+"px");
-          ltTable.css("marginLeft", -currentScrollLeft+"px");
-          topTable.css("marginTop", -currentScrollTop+"px");
-          topTable.css("marginLeft", ltTableWidth - currentScrollLeft+"px");
-          leftTable.css("marginTop", ltTableHeight - currentScrollTop+"px");
-          leftTable.css("marginLeft", -currentScrollLeft+"px");
-  
-        });
-  
-        // for IE, when window's scrollbar is moved, keep ltTable,leftTable,topTable in the correct position
-        $(window).resize(function(){
-          $(document).scroll();
-        });
-  
-        //reset the width of topTable and the height of leftTable when container's size is changed, if container's size is fixed, you can skip the code below.
-        timerLT = setInterval(function(){
-          if(containerWidth != container.width() || containerHeight != container.height()){
-            topTable.width(container.width() - ltTableWidth - (container.innerWidth() - container[0].clientWidth));
-            leftTable.height(container.height() - ltTableHeight - (container.innerHeight() - container[0].clientHeight));
-  
-            containerWidth = container.width();
-            containerHeight = container.height();
-  
-            container.scroll();// for IE
-              
-          };
-                     
-        },0);
-
+  export default {
+    data() {
+      return {}
+    },
+    props: [],
+    components: {},
   }
-}
 </script>
 <style lang="less" scoped>
-@import "../assets/stylesheets/base01.less";
-@import "../assets/stylesheets/element.less";
+  @import "../assets/stylesheets/base01.less";
+  @import "../assets/stylesheets/element.less";
 
-.container {
-  min-height: 100vh;
-  padding: 20px;
-  background-color: #fff;
-}
+  .container {
+    min-height: 100vh;
+    padding: 20px;
+    background-color: #fff;
+  }
 </style>
